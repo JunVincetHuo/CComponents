bec33eb79794966e152c80022e15c748e55e3199
Huo <junvincethuo@gmail.com>
2017-06-08 10:17:27 +0800

    goTop

---
+++ js/goTop.js
@@ -0,0 +1,32 @@
+function GoTop($ct){
+    this.$ct = $ct || $('body');
+    this.$target = $('<button class="goTopBtn">Go Top</button>');
+    this.createNode();
+    this.bindEvent();
+}
+GoTop.prototype = {
+    bindEvent:function(){
+        var _this = this;
+        this.$target.on('click',function(){
+            $(window).scrollTop(0);
+        })
+        $(window).on('scroll',function(){
+            $(this).scrollTop()>100?_this.$target.show():_this.$target.hide();
+        })
+    },
+    createNode:function(){
+        this.$ct.append(this.$target);
+        this.$target.hide();
+    }
+}
+
+var goTop = (function(){
+    return {
+        init:function(){
+            new GoTop()
+        }
+    }
+})()
+
+goTop.init();
+
